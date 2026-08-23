6e6dfde9f86b583ef358013bc3ca7011b71b0e56
Emir HAMURCU <134494889+WhoIsMrSentry@users.noreply.github.com>
2026-08-23 14:30:00 +0300

    refactor(hardware): remove deprecated raw hardware service in favor of modular serial

--- modules/hardware/__init__.py
+++
@@ -1,8 +0,0 @@
-# Hardware Abstraction Layer (HAL)
-# All services accept a ServiceClient and delegate to existing microservices via HTTP.
-from .services.servo_service import ServoService
-from .services.lights_service import LightsService
-from .services.motor_service import MotorService
-from .services.audio_service import AudioService
-
-__all__ = ["ServoService", "LightsService", "MotorService", "AudioService"]
