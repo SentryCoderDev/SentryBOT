7a31e69a9c1a17b3af07a950427507141b43467e
Emir HAMURCU <134494889+WhoIsMrSentry@users.noreply.github.com>
2026-09-01 16:30:00 +0300

    feat(camera): initialize device package structure for multi-sensor hardware

---
+++ modules/camera/devices/__init__.py
@@ -0,0 +1,9 @@
+from .device_lock import CameraMode, CameraConfig, DeviceState, DeviceRef, DeviceHandle
+
+__all__ = [
+    "CameraMode",
+    "CameraConfig",
+    "DeviceState",
+    "DeviceRef",
+    "DeviceHandle",
+]
